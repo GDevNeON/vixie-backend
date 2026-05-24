35447edb8099af8bde0b2eb4fd56e5cc9dac3060
GDevNeON <neongaming4ever@gmail.com>
2026-05-24 14:09:57 +0700

    Accept camelCase FCM token fields

--- user-auth/src/main/java/com/neong/vixie/models/dto/NotificationTokenRequest.java
+++ user-auth/src/main/java/com/neong/vixie/models/dto/NotificationTokenRequest.java
@@ -1,12 +1,15 @@
 package com.neong.vixie.models.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotBlank;
 
 /**
  * Request DTO for registering/updating an FCM token.
  */
 public record NotificationTokenRequest(
+        @JsonAlias("deviceId")
         @NotBlank String deviceId,
+        @JsonAlias("fcmToken")
         @NotBlank String fcmToken,
         @NotBlank String platform
 ) {}
